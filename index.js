e74e558405d983c5bd4f4f718e0c0f615059b304
Sorumond <sorumond@gmail.com>
2019-06-22 18:39:47 +0300

    commit

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ let gameField = document.querySelector('.game__field');
 
 [...gameField.children].forEach((square) => {
     let random = Math.random();
-    square.innerHTML = random < 0.5 ? '♠' : random < 0.6 ? '♣' : random < 0.75 ? '♢' : '♡';
+    square.innerHTML = random < 0.7 ? '♠' : random < 0.8 || random > 0.7 ? '♣' : random < 0.9 || random > 0.8 ? '♢' : '♡';
     square.setAttribute('x', x);
     x++;
     square.setAttribute('y', y);
-
-    console.log(square.getAttribute("x"));
     if (x > 6) {
         y++;
         x = 1;
@@ -23,6 +21,7 @@ function removeInner(x, y) {
     let bottomValue = gameField.querySelector(`[x="${x}"][y="${y + 1}"]`) ? gameField.querySelector(`[x="${x}"][y="${y + 1}"]`).innerHTML : 0;
     let leftValue = gameField.querySelector(`[x="${x - 1}"][y="${y}"]`) ? gameField.querySelector(`[x="${x - 1}"][y="${y}"]`).innerHTML : 0;
     let rightValue = gameField.querySelector(`[x="${x + 1}"][y="${y}"]`) ? gameField.querySelector(`[x="${x + 1}"][y="${y}"]`).innerHTML : 0;
+    gameField.querySelector(`[x="${x}"][y="${y}"]`).innerHTML = '';
 
     if (value === topValue) {
         removeInner(x, y - 1);
@@ -36,12 +35,13 @@ function removeInner(x, y) {
     if (value === rightValue) {
         removeInner(x + 1, y);
     }
-    gameField.querySelector(`[x="${x}"][y="${y}"]`).innerHTML = '';
 }
 
 document.addEventListener('click', (e) => {
-   const isGameSquares = e.target.matches('.game__squares');
-   if (isGameSquares) {
-       removeInner(e.target.getAttribute("x"), e.target.getAttribute("y"));
-   }
+    const isGameSquares = e.target.matches('.game__squares');
+    if (isGameSquares) {
+        let x = Number(e.target.getAttribute("x"));
+        let y = Number(e.target.getAttribute("y"));
+        removeInner(x, y);
+    }
 });
