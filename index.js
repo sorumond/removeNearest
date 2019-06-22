4c7a771242c199f7d6b9ed3395b426a3177121bc
Sorumond <sorumond@gmail.com>
2019-06-22 20:02:37 +0300

    format code

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 let x = 1;
 let y = 1;
-let gameField = document.querySelector('.game__field');
+let gameField = document.querySelector(".game__field");
 
-[...gameField.children].forEach((square) => {
+[...gameField.children].forEach(square => {
     let random = Math.random();
-    // square.innerHTML = random < 0.25 ? '♠' : random > 0.25 && random > 0.5 ? '♣' : random > 0.5 && random < 0.75 ? '♢' : '♡';
-
     if (random < 0.25) {
-        square.innerHTML = '♠';
-        square.style.background = 'red';
+        square.innerHTML = "♠";
+        square.style.background = "red";
     } else if (random > 0.25 && random < 0.5) {
-        square.innerHTML = '♣';
-        square.style.background = 'green';
-    } else if ( random > 0.5 && random < 0.75) {
-        square.innerHTML = '♢';
-        square.style.background = 'yellow';
+        square.innerHTML = "♣";
+        square.style.background = "green";
+    } else if (random > 0.5 && random < 0.75) {
+        square.innerHTML = "♢";
+        square.style.background = "yellow";
     } else {
-        square.innerHTML = '♡';
-        square.style.background = '#00BFFF';
+        square.innerHTML = "♡";
+        square.style.background = "#00BFFF";
     }
 
-    square.setAttribute('x', x);
+    square.setAttribute("x", x);
     x++;
-    square.setAttribute('y', y);
+    square.setAttribute("y", y);
     if (x > 6) {
         y++;
         x = 1;
@@ -30,17 +28,24 @@ let gameField = document.querySelector('.game__field');
 });
 
 function removeInner(x, y) {
-    console.count();
     let value = gameField.querySelector(`[x="${x}"][y="${y}"]`).innerHTML;
-    if (value === '') {
+    if (value === "") {
         return;
     }
-    let topValue = gameField.querySelector(`[x="${x}"][y="${y - 1}"]`) ? gameField.querySelector(`[x="${x}"][y="${y - 1}"]`).innerHTML : 0;
-    let bottomValue = gameField.querySelector(`[x="${x}"][y="${y + 1}"]`) ? gameField.querySelector(`[x="${x}"][y="${y + 1}"]`).innerHTML : 0;
-    let leftValue = gameField.querySelector(`[x="${x - 1}"][y="${y}"]`) ? gameField.querySelector(`[x="${x - 1}"][y="${y}"]`).innerHTML : 0;
-    let rightValue = gameField.querySelector(`[x="${x + 1}"][y="${y}"]`) ? gameField.querySelector(`[x="${x + 1}"][y="${y}"]`).innerHTML : 0;
-    gameField.querySelector(`[x="${x}"][y="${y}"]`).innerHTML = '';
-    gameField.querySelector(`[x="${x}"][y="${y}"]`).style.background = '';
+    let topValue = gameField.querySelector(`[x="${x}"][y="${y - 1}"]`)
+        ? gameField.querySelector(`[x="${x}"][y="${y - 1}"]`).innerHTML
+        : 0;
+    let bottomValue = gameField.querySelector(`[x="${x}"][y="${y + 1}"]`)
+        ? gameField.querySelector(`[x="${x}"][y="${y + 1}"]`).innerHTML
+        : 0;
+    let leftValue = gameField.querySelector(`[x="${x - 1}"][y="${y}"]`)
+        ? gameField.querySelector(`[x="${x - 1}"][y="${y}"]`).innerHTML
+        : 0;
+    let rightValue = gameField.querySelector(`[x="${x + 1}"][y="${y}"]`)
+        ? gameField.querySelector(`[x="${x + 1}"][y="${y}"]`).innerHTML
+        : 0;
+    gameField.querySelector(`[x="${x}"][y="${y}"]`).innerHTML = "";
+    gameField.querySelector(`[x="${x}"][y="${y}"]`).style.background = "";
     if (value === topValue) {
         removeInner(x, y - 1);
     }
@@ -53,11 +58,10 @@ function removeInner(x, y) {
     if (value === rightValue) {
         removeInner(x + 1, y);
     }
-
 }
 
-document.addEventListener('click', (e) => {
-    const isGameSquares = e.target.matches('.game__squares');
+document.addEventListener("click", e => {
+    const isGameSquares = e.target.matches(".game__squares");
     if (isGameSquares) {
         let x = Number(e.target.getAttribute("x"));
         let y = Number(e.target.getAttribute("y"));
